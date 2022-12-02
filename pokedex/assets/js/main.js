5663571272727317834b131fb7312b6885a94d0d
Bruna <brunamata@usp.br>
2022-12-02 10:45:08 -0300

    Concluindo procedimento de paginação

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,31 +1,56 @@
 const { json } = require("server/reply");
 
+const pokemonList = document.getElementById('pokemonList'); 
+const LoadMoreButton = document.getElementById("loadMoreButton");
+const maxRecords = 151;
+const limit = 5
+let offset = 0;
 
-function convertPokemonToLI(pokemon){
-    return '\
-    <li class="pokemon $(pokemon.type)"> \
-        <span class="number">$(pokemon.number)</span> \
-        <span class="name">$(pokemon.name)</span>\
-\
-        <div class="detail">  \
-            <ol class="types"> \
-                $(pokemon.types.map((type)=> "<li class="type">$(type)</li>").join(""))\
-            </ol>\
-\
-            <img src="$(pokemon.sprites.other.dream_world.front_default)" alt=$(pokemon.name)>\
-        </div>\
+
+
+function loadPokemonItens(offset, limit){
+
+    pokeApi.getPokemons(offset, limit).then((pokemons = [])=> {
+
+        pokemonList.innerHTML += pokemons.map((pokemon) => 
+         '  <li class="pokemon $(pokemon.type)"> \
+                <span class="number">$(pokemon.number)</span> \
+                <span class="name">$(pokemon.name)</span>\
         \
-    </li>\
-    '
+                <div class="detail">  \
+                    <ol class="types"> \
+                        $(pokemon.types.map((type)=> "<li class="type">$(type)</li>").join(""))\
+                    </ol>\
+        \
+                    <img src="$(pokemon.sprites.other.dream_world.front_default)" alt=$(pokemon.name)>\
+                </div>\
+                \
+            </li>\
+            '
+        ).join('');
+        //.map retorna uma lista transformada sem precisar da estrutura antiga do for
+        //.join junta todos os elementos em string com separador dentro do () - eh vazio, nesse caso
+    
+        })
 }
 
-const pokemonList = document.getElementById('pokemonList'); 
+loadPokemonItens(offset, limit); //logo q começa ja carrega com os dados atuais
 
-  pokeApi.getPokemons().then((pokemons = [])=> {
+LoadMoreButton.addEventListener('click', ()=> {
+    offset += limit;
+    const qtdRecordNexPage = offset + limit;
 
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLI).join("") 
-    //.map retorna uma lista transformada sem precisar da estrutura antiga do for
-    //.join junta todos os elementos em string com separador dentro do () - eh vazio, nesse caso
+    //remover botao quando ultrapassa a qtd maxima
+    if(qtdRecordNexPage >= maxRecords){ 
 
-    })
+        const newLimit = maxRecords - offset;
+        loadPokemonItens(offset, newLimit);
+
+        LoadMoreButton.parentElement.removeChild(LoadMoreButton);
+    }else{
+        loadPokemonItens (offset, limit); //se n atingiu o max continua carregando mais
+    }
+})
+
+
 
